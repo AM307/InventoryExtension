c44bcd6241aa084766ef3ac5b7a937a983b7e0d0
Florian Weiss <github@weissheiten.at>
2017-02-27 13:22:01 +0100

    change to fixed Extension name

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,9 +1,7 @@
 <?php
 
-call_user_func(function($packageKey) {
-    \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
-        'AM307.' . $packageKey,
-        'List',
-        'The Inventory List'
-    );
-}, $_EXTKEY);
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'AM307.Inventory',
+    'MATList',
+    'The MAT Inventory List'
+);
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,8 +4,8 @@ if (!defined ('TYPO3_MODE')) die ('Access denied.');
 call_user_func(
     function($extKey) {
         \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-            'AM307.' .$extKey,
-            'List',
+            'AM307.Inventory',
+            'MATList',
             array('Inventory' => 'list')
         );
     },
